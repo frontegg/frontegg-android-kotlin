4c5283aaddb9c858cfa077afc6d05a0b676c7367
David Antoon <david@frontegg.com>
2023-05-04 01:17:19 +0300

    add test runner script

---
+++ run-tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./gradlew :app:connectedCheck -Pandroid.testInstrumentationRunnerArguments.class=com.frontegg.demo.LoginWithPasswordTest
+./gradlew :app:connectedCheck -Pandroid.testInstrumentationRunnerArguments.class=com.frontegg.demo.loginWithSAMLTest
