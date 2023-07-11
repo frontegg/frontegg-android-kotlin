1640ac46fd2eecb570cecacb853cddc368fc39ee
David Antoon <david@frontegg.com>
2023-07-11 15:37:52 +0300

    add authentication activity to be able to launch a frontegg oauth from react native applicaiton

--- android/src/main/java/com/frontegg/android/FronteggAuth.kt
+++ android/src/main/java/com/frontegg/android/FronteggAuth.kt
@@ -113,6 +113,8 @@ class FronteggAuth(
 
             refreshTaskRunner?.cancel()
 
+
+
             if (decoded.exp > 0) {
                 val now: Long = Instant.now().toEpochMilli()
                 val offset = (((decoded.exp * 1000) - now) * 0.80).toLong()
--- app/src/main/java/com/frontegg/demo/FirstFragment.kt
+++ app/src/main/java/com/frontegg/demo/FirstFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.frontegg.android.FronteggApp
 import com.frontegg.android.FronteggAuth
 import com.frontegg.demo.databinding.FragmentFirstBinding
 import io.reactivex.rxjava3.disposables.Disposable
