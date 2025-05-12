e164b3d1b6707d222fcafd05c438b70055fa0ded
alex-minaiev-frontegg <alex.minaiev@frontegg.com>
2025-05-12 10:27:42 +0300

    Improved FronteggInnerStorage

--- android/src/main/java/com/frontegg/android/services/FronteggInnerStorage.kt
+++ android/src/main/java/com/frontegg/android/services/FronteggInnerStorage.kt
@@ -1,44 +1,48 @@
 package com.frontegg.android.services
 
-import android.util.Log
 import com.frontegg.android.regions.RegionConfig
 
 class FronteggInnerStorage {
     val baseUrl: String
-        get() = data["baseUrl"] as String
+        get() = data["baseUrl"] as String? ?: ""
     val clientId: String
-        get() = data["clientId"] as String
+        get() = data["clientId"] as String? ?: ""
     val applicationId: String?
         get() = data["applicationId"] as String?
 
     val isEmbeddedMode: Boolean
-        get() = data["isEmbeddedMode"] as Boolean
+        get() = data["isEmbeddedMode"] as Boolean? ?: true
     val regions: List<RegionConfig>
         get() {
-            return data["regions"] as List<RegionConfig>? ?: return listOf()
+            if (data["regions"] is List<*>) {
+                val regions = data["regions"] as List<*>
+                return regions.filterIsInstance<RegionConfig>()
+            }
+
+            return listOf()
         }
     val selectedRegion: RegionConfig?
         get() = data["selectedRegion"] as RegionConfig?
     val handleLoginWithSocialLogin: Boolean
-        get() = data["handleLoginWithSocialLogin"] as Boolean
+        get() = data["handleLoginWithSocialLogin"] as Boolean? ?: true
     val handleLoginWithSocialLoginProvider: Boolean
-        get() = data["handleLoginWithSocialLoginProvider"] as Boolean
+        get() = data["handleLoginWithSocialLoginProvider"] as Boolean? ?: true
     val handleLoginWithCustomSocialLoginProvider: Boolean
-        get() = data["handleLoginWithCustomSocialLoginProvider"] as Boolean
+        get() = data["handleLoginWithCustomSocialLoginProvider"] as Boolean? ?: true
     val customUserAgent: String?
         get() = data["customUserAgent"] as String?
     val handleLoginWithSSO: Boolean
-        get() = data["handleLoginWithSSO"] as Boolean
+        get() = data["handleLoginWithSSO"] as Boolean? ?: false
     val shouldPromptSocialLoginConsent: Boolean
-        get() = data["shouldPromptSocialLoginConsent"] as Boolean
+        get() = data["shouldPromptSocialLoginConsent"] as Boolean? ?: true
     val useAssetsLinks: Boolean
-        get() = data["useAssetsLinks"] as Boolean
+        get() = data["useAssetsLinks"] as Boolean? ?: false
     val useChromeCustomTabs: Boolean
-        get() = data["useChromeCustomTabs"] as Boolean
+        get() = data["useChromeCustomTabs"] as Boolean? ?: false
     val mainActivityClass: Class<*>?
         get() = data["mainActivityClass"] as Class<*>?
     val packageName: String
-        get() = data["packageName"] as String
+        get() = data["packageName"] as String? ?: ""
     val deepLinkScheme: String?
         get() = data["deepLinkScheme"] as String?
 
