ffb56f1f756ffb3c13d1993806026bbd3f5f6f19
dianaKhortiuk-frontegg <diana.khortiuk@frontegg.com>
2025-09-18 18:07:42 +0200

    Social login redirecting update

--- android/src/main/java/com/frontegg/android/models/SocialLoginPostLoginRequest.kt
+++ android/src/main/java/com/frontegg/android/models/SocialLoginPostLoginRequest.kt
@@ -3,22 +3,6 @@ package com.frontegg.android.models
 import com.google.gson.annotations.SerializedName
 
 /**
- * Model for social login callback data
- */
-data class SocialLoginCallback(
-    @SerializedName("action")
-    val action: String,
-    @SerializedName("provider")
-    val provider: String,
-    @SerializedName("appId")
-    val appId: String,
-    @SerializedName("bundleId")
-    val bundleId: String,
-    @SerializedName("platform")
-    val platform: String
-)
-
-/**
  * Model for social login post-login request
  */
 data class SocialLoginPostLoginRequest(
